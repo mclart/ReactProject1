b6ad4e5dec883293aaecd6954d6114d3ecda07f5
mclart <mclart@alumnes.santjosepobrer.es>
2025-03-10 16:18:05 +0100

    React instalat. Carpeta About feta

---
+++ projecte_react/src/app/about/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function aboutPage() {
+  return (
+    <div>
+      <p>hola caracola. He creat un /src/about/page.tsx i se mostra aixo</p>
+
+    </div>
+  )
+}
+
+export default aboutPage
---
+++ projecte_react/src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Pantalla Principal</h1>
+    </div>
+  );
+}
